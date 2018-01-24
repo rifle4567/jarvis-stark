c7202c0bccaef5608204fa35e96aab4e422d3833
rifle4567 <35484167+rifle4567@users.noreply.github.com>
2018-01-24 12:30:12 +0530

    Add files via upload

---
+++ random 1.py
@@ -0,0 +1,36 @@
+count =0
+
+while int(count<=100):
+    i=input("press r to roll a dice")
+    if="r":
+        a=random.randunt(1,6)
+        count=count+a        print(a,count)
+        if count==8:
+        count=37
+        print("its a ladder,come up")
+        count=count+a
+        elif count==52:
+        count=81
+        print("its a ladder,come up")
+        count=count+a
+        elif count==40:
+        count=68
+        print("its a ladder,come up")
+        count=count+a
+        elif count==76:
+        count=97
+        print("its a ladder,come up")
+        count=count+a
+        elif count==11:
+            count=2
+            print("snake bite,come down")
+            count=count+a
+            elif count==25:
+            count=4
+            print("snake bite,come down")
+            count=count+a
+            elif count==65:
+                count=46
+                print("snake bite,come down")
+                count=count+a
+        
