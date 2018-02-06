c496aea8965977dd01e76e4a7182a79d3dab0d9b
rifle4567 <35484167+rifle4567@users.noreply.github.com>
2018-02-06 15:38:43 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,21 @@
+a = 5
+print(a, "is of type", type(a))
+
+a = 2.0
+print(a, "is of type", type(a))
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
+
+s = 'Hello world!'
+
+# s[4] = 'o'
+print("s[4] = ", s[4])
+
+# s[6:11] = 'world'
+print("s[6:11] = ", s[6:11])
+
+# Generates error
+# Strings are immutable in Python
+s[5] ='d'
+
